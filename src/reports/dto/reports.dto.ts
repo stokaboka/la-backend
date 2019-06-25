21f6ff70035d8405ba0fba5e323285d796872068
stokaboka <igothorev@gmail.com>
2019-06-25 16:45:55 +0300

    refactor: add reports.dto

---
+++ src/reports/dto/reports.dto.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018.  Igor Khorev, Orangem.me, igorhorev@gmail.com
+ */
+
+export class ReportsDto {
+  readonly id: number;
+  readonly idUser: number;
+  readonly attempt: number;
+  readonly dt: Date;
+  readonly test: number;
+  readonly type: string;
+  readonly data: object;
+}
