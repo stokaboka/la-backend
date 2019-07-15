47a065dbcb3848b2f7225e5a31238642d2e46cc4
stokaboka <igothorev@gmail.com>
2019-07-15 22:43:06 +0300

    refactor: remove services from app.module, delete comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TokensModule } from './tokens/tokens.module';
 import { QuestionsController } from './questions/questions.controller';
-// import { QuestionsService } from './questions/questions.service';
 import { QuestionsModule } from './questions/questions.module';
 import { DescriptionsModule } from './descriptions/descriptions.module';
 import { DescriptionsController } from './descriptions/descriptions.controller';
-// import { DescriptionsService } from './descriptions/descriptions.service';
 import { ResultsController } from './results/results.controller';
-// import { ResultsService } from './results/results.service';
 import { AttemptsController } from './attempts/attempts.controller';
 import { ResultsModule } from './results/results.module';
 import { ReportsModule } from './reports/reports.module';
@@ -22,12 +19,9 @@ import { ConfigModule } from './config/config.module';
 import { ClientModule } from './client/client.module';
 import { LevelsModule } from './levels/levels.module';
 import { LevelsController } from './levels/levels.controller';
-// import { LevelsService } from './levels/levels.service';
 import { OrdersModule } from './orders/orders.module';
-// import { OrdersService } from './orders/orders.service';
 import { OrderDetailsModule } from './order-details/order-details.module';
 import { CoursesModule } from './courses/courses.module';
-// import { CoursesService} from './courses/courses.service';
 
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
@@ -69,12 +63,6 @@ import { JwtModule } from '@nestjs/jwt';
   providers: [
     AppService,
     AuthService,
-    // QuestionsService,
-    // ResultsService,
-    // LevelsService,
-    // DescriptionsService,
-    // OrdersService,
-    // CoursesService,
   ],
   exports: [
     JwtModule,
@@ -84,10 +72,7 @@ import { JwtModule } from '@nestjs/jwt';
     ResultsModule,
     LevelsModule,
     DescriptionsModule,
-    // DescriptionsService,
     ConfigModule,
-    // OrdersService,
-    // CoursesService,
   ],
 })
 export class AppModule {
