03f1c3cd2dce7313ab8bbf02bb1c33b92970fc9e
stokaboka <igothorev@gmail.com>
2019-07-29 15:31:01 +0300

    refactor: route /descriptions/test/:test/results/:results no auth control

--- src/descriptions/descriptions.controller.ts
+++ src/descriptions/descriptions.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, UseGuards, Get, Param } from '@nestjs/common';
 import { DescriptionsService } from './descriptions.service';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+// import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { DescriptionFind } from './dto/description.find.dto';
 
 @Controller('descriptions')
@@ -8,7 +8,7 @@ export class DescriptionsController {
   constructor(readonly descriptionsService: DescriptionsService) {}
 
   @Get('test/:test/results/:results')
-  @UseGuards(new JwtAuthGuard())
+  // @UseGuards(new JwtAuthGuard())
   async findOne(@Param() params: DescriptionFind): Promise<any[]> {
     return this.descriptionsService.findDescriptionsResults(params);
   }
